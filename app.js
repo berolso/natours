d47dd7c1124e30eeaa7ebfc7e82086b5d5a5564e
berolso <insta11@hotmail.com>
2019-08-31 13:49:58 -0700

    52. Starting Our API: Handling GET Requests

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('you can post to this endpoint..');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get(`/api/v1/tours`, (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`running on port ${port}...`);
+});

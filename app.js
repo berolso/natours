54646a7b0835dfad71d6527b8e26d7c817b06fc7
berolso <insta11@hotmail.com>
2019-08-31 22:21:07 -0700

    56. Handling DELETE Requests

--- app.js
+++ app.js
@@ -88,6 +88,21 @@ app.patch(`/api/v1/tours/:id`, (req, res) => {
   });
 });
 
+//56
+app.delete(`/api/v1/tours/:id`, (req, res) => {
+  // if (!tour) {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id'
+    });
+  }
+  res.status(204).json({
+    status: 'sucess',
+    data: null
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`running on port ${port}...`);

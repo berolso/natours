634e45204135f2c53b7ac7cca69649ae07a216bb
berolso <insta11@hotmail.com>
2019-08-31 22:15:09 -0700

    55. Handling PATCH Requests

--- app.js
+++ app.js
@@ -35,15 +35,13 @@ app.get(`/api/v1/tours/:id`, (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
 
-//   if (id > tours.length) {
+  //   if (id > tours.length) {
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
       message: 'invalid id'
     });
   }
-
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -71,6 +69,23 @@ app.post(`/api/v1/tours`, (req, res) => {
       });
     }
   );
+});
+
+//55
+app.patch(`/api/v1/tours/:id`, (req, res) => {
+  // if (!tour) {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id'
+    });
+  }
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      tour: '<updated tour her...>'
+    }
+  });
 });
 
 const port = 3000;

11c23b55d2e1127798afb0dba030aafb001eea41
berolso <insta11@hotmail.com>
2019-09-04 12:12:05 -0700

    124. Modelling Users

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose'); //124
+const validator = require('validator'); //124
+//124
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password!']
+  }
+});
+//124
+const User = mongoose.model('User', userSchema);
+//124
+module.exports = User;

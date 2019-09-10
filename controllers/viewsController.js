7b72ccc6b97c6a9ff5f47401c34e323ee6489f30
berolso <insta11@hotmail.com>
2019-09-10 01:05:02 -0700

    181. Building the Tour Overview - Part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,18 @@
-//180
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel'); //181
+const catchAsync = require('../utils/catchAsync'); //181
+
+//180, 181
+exports.getOverview = catchAsync(async (req, res) => {
+  //1 get tour data from collection
+  const tours = await Tour.find();
+  //2 build template
+
+  //3 render that template using tour data from step 1
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 //180
 exports.getTour = (req, res) => {

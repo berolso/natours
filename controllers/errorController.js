a93d4968313bb746b55e88e216bd0ccb80f5a4b1
berolso <insta11@hotmail.com>
2019-09-10 23:52:17 -0700

    192. Rendering Error Pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -23,45 +23,77 @@ const handleJWTError = () =>
 //131
 const handleJWTExpiredError = () =>
   new AppError('Expired token. Please log in again!', 401);
+
 //117
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
+const sendErrorDev = (err, req, res) => {
+  //192
+  // A) api
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  }
+  // B) rendered website
+  console.error('ERROR Shit!!', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: err.message
   });
 };
-//117
-const sendErrorProd = (err, res) => {
-  //Operational, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-    //Programming or other unknown error: don't leak error details
-  } else {
+
+//117, 192
+const sendErrorProd = (err, req, res) => {
+  // A) api
+  if (req.originalUrl.startsWith('/api')) {
+    // A) Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    }
+    // B) Programming or other unknown error: don't leak error details
     //1) Log error
     console.error('ERROR Shit!!', err);
     //2) Send generic message
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
-      message: 'something went very wrong'
+      message: 'Something went very wrong!'
     });
   }
+  // B) rendered website
+  // A) Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong',
+      msg: err.message
+    });
+  }
+  // B) Programming or other unknown error: don't leak error details
+  //1) Log error
+  console.error('ERROR Shit!!', err);
+  //2) Send generic message
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: 'Please try again later.'
+  });
 };
 
 //114
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  //117
+  //117, 192
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     //118
     let error = { ...err };
+    //192
+    error.message = err.message;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error); //118
     if (error.code === 11000) error = handleDuplicateFieldsDB(error); //119
@@ -70,6 +102,6 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel'); //181
 const catchAsync = require('../utils/catchAsync'); //181
+const AppError = require('../utils/appError'); //192
 
 //180, 181
 exports.getOverview = catchAsync(async (req, res) => {
@@ -14,13 +15,18 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-//180, 185
+//180, 185, 192
 exports.getTour = catchAsync(async (req, res, next) => {
   //1 get data for requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user photo'
   });
+
+  // // 192
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
   //2 build template
 
   //3 render template using data from step 1

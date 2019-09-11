752317c05711a0b99e530ff23813cbaa75274486
berolso <insta11@hotmail.com>
2019-09-11 14:34:53 -0700

    207. Sending Password Reset Emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -168,7 +168,7 @@ exports.restrictTo = (...roles) => {
   };
 };
 
-//134
+//134, 207
 exports.forgotPassword = catchAsync(async (req, res, next) => {
   //1) Get user based on posted token
   const user = await User.findOne({ email: req.body.email });
@@ -179,17 +179,13 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
   //3) Send it to user's email
-  //135
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-  const message = `Forgot your password? submit a PATCH request with your new password and passwordConfirm to: ${resetURL}. \nIf you didn't forget your password, please ignore this email`;
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: 'Your password reset token (valid for 10 min)',
-    //   message
-    // });
+    //135
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    //207
+    await new Email(user, resetURL).sendPasswordReset();
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!'
--- utils/email.js
+++ utils/email.js
@@ -48,4 +48,12 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family');
   }
+
+  //207
+  async sendPasswordReset() {
+    await this.send(
+      `passwordReset`,
+      `Your password reset token (valid for only 10 minutes)`
+    );
+  }
 };

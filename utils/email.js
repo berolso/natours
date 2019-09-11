1ac94c2eafede1a5945aae529d7c240bdc72a704
berolso <insta11@hotmail.com>
2019-09-11 13:32:21 -0700

    205. Building a Complex Email Handler

--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,54 @@
 const nodemailer = require('nodemailer'); //135
-//135
-const sendEmail = async options => {
-  //1) create a transporter
-  const transporter = nodemailer.createTransport({
-    // service: 'Gmail',
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+const pug = require('pug'); //205
+const htmlToText = require('html-to-text'); //205
+
+//205
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `test <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //sendgrid
+      return 1;
     }
-    //Activate in gmail 'less secure app' option
-  });
-  //2) define the email options
-  const mailOptions = {
-      from: 'test <hi@hi.com>',
-      to: options.email,
-      subject: options.subject,
-      text: options.message
-      // html:
+    return nodemailer.createTransport({
+      // service: 'Gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
+  //send the actual email
+  async send(template, subject) {
+    //1) render html based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject
+      }
+    );
+    //2) define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
     };
-    // 3) actually send the email
-    console.log(mailOptions);
-  await transporter.sendMail(mailOptions);
+    // 3) create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family');
+  }
 };
-// 135
-module.exports = sendEmail;

f3546248089d755646e777afdde4bfed13e5c2cf
berolso <insta11@hotmail.com>
2019-09-11 03:45:46 -0700

    200. Saving Image Name to Database

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,6 +60,8 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   //2 filtered out unwanted field names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  //200
+  if (req.file) filteredBody.photo = req.file.filename;
   //3 update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
--- models/userModel.js
+++ models/userModel.js
@@ -16,7 +16,11 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email']
   },
-  photo: String,
+  photo: {
+    //200
+    type: String,
+    default: 'default.jpg'
+  },
   //133
   role: {
     type: String,

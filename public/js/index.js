0fd48894fbfa5be636d6c28d55cecffc0a63bdc2
berolso <insta11@hotmail.com>
2019-09-11 01:39:12 -0700

    195. Updating User Data with Our API

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,14 @@
 import '@babel/polyfill'; //190
 import { displayMap } from './mapbox'; //190
 import { login, logout } from './login'; //190
+import { updateData } from './updateSettings'; //195
 
 //dom elements
 // 190
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login'); //194
-//191
-const logOutBtn = document.querySelector('.nav__el--logout');
+const logOutBtn = document.querySelector('.nav__el--logout'); //191
+const userDataForm = document.querySelector('.form-user-data'); //195
 
 //deligation
 //185, 190
@@ -29,3 +30,12 @@ if (loginForm) {
 
 //191
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+//195
+if (userDataForm)
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    updateData(name, email);
+  });
---
+++ public/js/updateSettings.js
@@ -0,0 +1,23 @@
+//195
+import axios from 'axios'; //195
+import { showAlert } from './alerts'; //195
+
+//update data
+
+export const updateData = async (name, email) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
+      data: {
+        name,
+        email
+      }
+    });
+    if (res.data.status === 'success') {
+      showAlert('success', 'Data updated successfully');
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};

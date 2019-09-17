54929517964c8dafe5ffc6ca56129d782967de9b
berolso <insta11@hotmail.com>
2019-09-16 22:53:37 -0700

    226. message bug fix

--- public/js/index.js
+++ public/js/index.js
@@ -88,5 +88,5 @@ if (bookBtn)
   });
 
 //226
-const alert = document.querySelector('body').dataset.alert;
-if (alert) showAlert('success', alertMessage, 20);
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);

5f92781ba62aa4c4915372a1b1e234d0dd286ea2
berolso <insta11@hotmail.com>
2019-09-17 01:44:50 -0700

    shorten alert time to 10 sec & change app name

--- public/js/index.js
+++ public/js/index.js
@@ -89,4 +89,4 @@ if (bookBtn)
 
 //226
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 20);
+if (alertMessage) showAlert('success', alertMessage, 10);
